5e70c3ec72052873bd859a5622f5fcfa516c181c
Don <don.j.olmstead@gmail.com>
2018-08-11 14:57:16 -0700

    docs: Add client example

---
+++ example/client.dart
@@ -0,0 +1,23 @@
+// Copyright (c) 2018 the HTTP Dart Project authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:async';
+
+import 'package:http_next/http.dart' as http;
+
+Future<void> main() async {
+  const url = 'https://httpbin.org/post';
+  final client = http.Client();
+  final response = await client.post(url, <String, String>{
+    'name': 'doodle',
+    'color': 'blue',
+  });
+  final responseText = await response.readAsString();
+
+  print(response.statusCode);
+  print(responseText);
+
+  client.close();
+}
