573a0d46df245c4e57f7313fd89eada9c08bf97e
Don <don.j.olmstead@gmail.com>
2020-10-02 13:57:39 -0700

    feat: Add extension for controlling BrowserClient

---
+++ lib/browser_client.dart
@@ -0,0 +1,7 @@
+// Copyright (c) 2020, the HTTP Dart project authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
+export 'src/browser_client.dart';
+export 'src/browser_client_context.dart';
