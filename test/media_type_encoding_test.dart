f38e40288770371851405f523e406bb6e6bb7055
Don <don.j.olmstead@gmail.com>
2021-01-04 08:24:51 -0800

    test: Add tests for MediaTypeEncoding

---
+++ test/media_type_encoding_test.dart
@@ -0,0 +1,58 @@
+// Copyright (c) 2021, the HTTP Dart project authors.
+// Please see the AUTHORS file for details. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE file.
+
+import 'dart:convert';
+
+import 'package:http_parser/http_parser.dart';
+import 'package:test/test.dart';
+
+import 'package:http_next/src/media_type_encoding.dart';
+
+MediaType _text() => MediaType('text', 'plain');
+
+MediaType _textWithCharset(String charset) =>
+    MediaType('text', 'plain', <String, String>{'charset': charset});
+
+void main() {
+  group('encoding', () {
+    test('none', () {
+      final mediaType = _text();
+      expect(mediaType.encoding, isNull);
+    });
+    test('unknown', () {
+      final mediaType = _textWithCharset('not-a-charset');
+      expect(mediaType.encoding, isNull);
+    });
+    test('ascii', () {
+      final mediaType = _textWithCharset(ascii.name);
+      expect(mediaType.encoding, ascii);
+    });
+    test('latin', () {
+      final mediaType = _textWithCharset(latin1.name);
+      expect(mediaType.encoding, latin1);
+    });
+    test('utf8', () {
+      final mediaType = _textWithCharset(utf8.name);
+      expect(mediaType.encoding, utf8);
+    });
+  });
+  group('changeEncoding', () {
+    test('ascii', () {
+      final mediaType = _text().changeEncoding(ascii);
+      expect(mediaType.parameters['charset'], ascii.name);
+      expect(mediaType.encoding, ascii);
+    });
+    test('latin', () {
+      final mediaType = _text().changeEncoding(latin1);
+      expect(mediaType.parameters['charset'], latin1.name);
+      expect(mediaType.encoding, latin1);
+    });
+    test('utf8', () {
+      final mediaType = _text().changeEncoding(utf8);
+      expect(mediaType.parameters['charset'], utf8.name);
+      expect(mediaType.encoding, utf8);
+    });
+  });
+}
